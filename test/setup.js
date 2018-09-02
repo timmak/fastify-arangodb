8ed82cd4987fe28a7b84a17a0964d933f48c2631
Tim Pinington <timpinington@gmail.com>
2018-09-02 15:27:11 +0100

    fix: update setup script

--- test/setup.js
+++ test/setup.js
@@ -1,6 +1,7 @@
 const arango = require("arangojs");
 const DB = new arango.Database();
 
+DB.useBasicAuth("root", "root");
 DB.createDatabase("test")
   .then(() => console.log("created test database"))
   .catch(() => console.log("unable to create test database"));
